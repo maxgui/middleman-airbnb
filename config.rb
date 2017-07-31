f45bf73f319825d0a4fec51bc2019850548d18c5
Maxime Guillemain <maxguillemain@gmail.com>
2017-07-31 17:02:09 +0200

    flats added

--- config.rb
+++ config.rb
@@ -20,3 +20,9 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["maxgui", "thedreamytoad", "monsieurpaillard", "papillard"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
+
+
